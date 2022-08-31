39e6b26ad483ce17ea77990e27ee703d040fd434
Guillaume M <gmeigniez@manadge.com>
2022-08-31 17:24:38 +0200

    Fix network adding node position bug

--- src/app/periples/periple-profile/periple-network/periple-network.component.ts
+++ src/app/periples/periple-profile/periple-network/periple-network.component.ts
@@ -1,9 +1,7 @@
 import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import * as d3 from 'd3';
-import { drag } from "d3-drag";
 import { NetworkService } from 'src/app/services/network.service';
-import { NetworkNode, Periple, PositionnedNode } from 'src/app/shared/models/network';
-import { v4 as uuidv4 } from 'uuid';
+import { NetworkLink, NetworkNode, Periple, PositionnedNode } from 'src/app/shared/models/network';
 
 @Component({
   selector: 'app-periple-network',
@@ -35,6 +33,7 @@ export class PeripleNetworkComponent implements OnInit, OnChanges {
     if (changes['network'] && changes['network']['currentValue']) {
 
       this.positionnedNodes = this.setInitialPosition(this.network?.nodes || []);
+      this.initDragHandler();
 
       setTimeout(() => {
         this.initSvg();
@@ -42,10 +41,9 @@ export class PeripleNetworkComponent implements OnInit, OnChanges {
         this.mainGroup = this.svg.append('g').attr('class', 'per-group');
         this.linksGroup = this.mainGroup.append('g').attr('class', 'per-links');
         this.nodesGroup = this.mainGroup.append('g').attr('class', 'per-nodes');
-        
-        this.updateNodes(this.positionnedNodes);
-        this.initLinks(this.linksGroup);
-        this.initZoom(this.mainGroup);
+
+        this.updateNodes();
+        this.updateLinks();
       });
     }
   }
@@ -64,127 +62,75 @@ export class PeripleNetworkComponent implements OnInit, OnChanges {
     if (this.svg) {
       this.svg.remove();
     }
-    this.width = this.getSVGBoundingRect()?.width || 0;
-    this.height = this.getSVGBoundingRect()?.height || 0;
-    
+    const rect: DOMRect | undefined = (this.el.nativeElement as HTMLElement).querySelector('#canvas')?.getBoundingClientRect();
+    this.width = rect?.width || 0;
+    this.height = rect?.height || 0;
+
     this.svg = d3.select(this.el?.nativeElement)
-    .select(".periple-network-container > #canvas")
-    .append("svg")
-    .attr('width', `100%`)
-    .attr('height', `100%`);
+      .select(".periple-network-container > #canvas")
+      .append("svg")
+      .attr('width', `100%`)
+      .attr('height', `100%`);
 
     this.svg.append('rect')
-    .attr('width', `100%`)
-    .attr('height', `100%`).attr('fill', 'none')
-    .attr('x', 0).attr('y', 0).attr('pointer-events', 'visible')
-    .attr('class', 'periple-network-background')
-    .on('click', (event: PointerEvent) => {
-      this.createNode({x: event.offsetX, y: event.offsetY});
-    })
+      .attr('width', `100%`)
+      .attr('height', `100%`).attr('fill', 'none')
+      .attr('x', 0).attr('y', 0).attr('pointer-events', 'visible')
+      .attr('class', 'periple-network-background')
+      .on('click', (event: PointerEvent) => {
+        const newNode: PositionnedNode = this.networkService.createNode(event);
+        const newLink: NetworkLink = this.networkService.createLink(newNode.id, this.positionnedNodes[0].id);
+        this.positionnedNodes.push(newNode);
+        this.network?.links.push(newLink);
+
+        this.updateNodes();
+        this.updateLinks();
+      })
   }
 
-  private updateNodes = (data: PositionnedNode[]): void => {
-    if (this.dragHandler) {
-      this.nodesGroup?.selectAll(".per-node").call(
-        this.dragHandler.on('start', null).on('drag', null).on('end', null)
+  private initDragHandler = () => {
+    this.dragHandler
+      .on('start', (event, d) => {
+        this.networkService.dragstarted(this.nodesGroup?.select(`.per-node#_${d.id}`).node());
+      })
+      .on('drag', (event: DragEvent, d: PositionnedNode) => {
+        const newPosition: { x: number, y: number } = { x: event.x, y: event.y };
+        d.x = event.x;
+        d.y = event.y;
+        this.networkService.dragged(this.nodesGroup?.select(`.per-node#_${d.id}`).node(), newPosition);
+        this.updateLinks();
+      })
+      .on('end', (event, d) => {
+        this.networkService.dragended(this.nodesGroup?.select(`.per-node#_${d.id}`).node())
+      });
+  }
+
+  private updateNodes = (): void => {
+    this.nodesGroup?.selectAll('.per-node').data((this.positionnedNodes || []))
+      .join(
+        function (enter) {
+          return enter.append('foreignObject').attr('class', 'per-node').attr('id', d => `_${d.id}`)
+            .attr('x', d => `${d.x}px`).attr('y', d => `${d.y}px`)
+            .append("xhtml:div")
+            .html(d => `<div class="per-node-html-container"><img src="${d.album.coverUrl}"></div>`);
+        },
+        function (update) {
+          return update.select('per-node')
+            .attr('x', d => `${d.x}px`).attr('y', d => `${d.y}px`);
+        }
+      ).call(
+        this.dragHandler
       );
-    }
-    
-    this.nodesGroup?.selectAll('.per-node').data((data || []))
-    .join(
-      function(enter) {
-        return enter.append('foreignObject').attr('class', 'per-node').attr('id', d => `_${d.id}`)
-        .attr('x', d => `${d.x}px`).attr('y', d => `${d.y}px`)
-        .append("xhtml:div")
-        .html(d => `<div class="per-node-html-container"><img src="${d.album.coverUrl}"></div>`);
-      },
-      function(update) {
-        return update.select('per-node')
-        .attr('x', d => `${d.x}px`).attr('y', d => `${d.y}px`);
-      }
-    ).call(
-      this.dragHandler
-        .on('start', (event, d) => {
-          this.networkService.dragstarted(this.nodesGroup?.select(`.per-node#_${d.id}`).node());
-        })
-        .on('drag', (event: DragEvent, d: PositionnedNode) => {
-          this.networkService.dragged(this.nodesGroup?.select(`.per-node#_${d.id}`).node(), event);
-          this.updateLinksPositions(data);
-        })
-        .on('end', (event, d) => this.networkService.dragended(this.nodesGroup?.select(`.per-node#_${d.id}`).node()))
-    );;
   }
 
-  private initLinks = (group: d3.Selection<any, any, any, any>): void => {
-    group.selectAll('.per-link').data((this.network?.links || [])).enter()
-    .append('line').attr('class', 'per-link');
+  private updateLinks = (): void => {
+    this.linksGroup?.selectAll('.per-link').data((this.network?.links || [])).enter()
+      .append('line').attr('class', 'per-link');
 
-    this.updateLinksPositions(this.positionnedNodes);
-  }
-
-  private initZoom = (group: d3.Selection<any, any, any, any> | null): void => {
-  this.svg.call(d3.zoom().translateExtent([[-500, -500], [this.width + 500, this.height + 500]])
-    .scaleExtent([0.6, 1])
-    .on("zoom", (e) => {
-      group?.attr('transform', e.transform)
-    }));
-  }
-
-  private getSVGBoundingRect = (): DOMRect | null => {
-    return (this.el.nativeElement as HTMLElement).querySelector('#canvas')?.getBoundingClientRect() || null;
-  }
-
-  private updateLinksPositions = (nodes: PositionnedNode[]): void => {
     this.svg.select('.per-links').selectAll('.per-link')
-    .attr('x1', (d: any) => {
-      const linkNodes: {from: PositionnedNode | undefined, to: PositionnedNode | undefined} = this.networkService.getLinkNodes(nodes, d);
-      if (!linkNodes.from || !linkNodes.to) {
-        return 0;
-      }
-      const fromNode: PositionnedNode = linkNodes.from;
-      const toNode: PositionnedNode = linkNodes.to;
-      return `${fromNode?.x + (this.MEDIA_SIZE / 2)}px`;
-    })
-    .attr('y1', (d: any) => {
-      const linkNodes: {from: PositionnedNode | undefined, to: PositionnedNode | undefined} = this.networkService.getLinkNodes(nodes, d);
-      if (!linkNodes.from || !linkNodes.to) {
-        return 0;
-      }
-      const fromNode: PositionnedNode = linkNodes.from;
-      const toNode: PositionnedNode = linkNodes.to;
-      return `${fromNode?.y + (this.MEDIA_SIZE / 2)}px`;
-    })
-    .attr('x2', (d: any) => {
-      const linkNodes: {from: PositionnedNode | undefined, to: PositionnedNode | undefined} = this.networkService.getLinkNodes(nodes, d);
-      if (!linkNodes.from || !linkNodes.to) {
-        return 0;
-      }
-      const fromNode: PositionnedNode = linkNodes.from;
-      const toNode: PositionnedNode = linkNodes.to;
-      return `${toNode?.x + (this.MEDIA_SIZE / 2)}px`;
-    })
-    .attr('y2', (d: any) => {
-      const linkNodes: {from: PositionnedNode | undefined, to: PositionnedNode | undefined} = this.networkService.getLinkNodes(nodes, d);
-      if (!linkNodes.from || !linkNodes.to) {
-        return 0;
-      }
-      const fromNode: PositionnedNode = linkNodes.from;
-      const toNode: PositionnedNode = linkNodes.to;
-      return `${toNode?.y + (this.MEDIA_SIZE / 2)}px`;
-    });
-  }
-
-  private createNode = (position: {x: number, y: number}): void => {
-    const id: string = uuidv4();
-    const newNode: NetworkNode = {
-      album: {
-        name: 'Unknown',
-        coverUrl: 'https://i.scdn.co/image/ab67616d00001e0285e087e2715d317c33f71a31'
-      },
-      id: id
-    }
-    this.positionnedNodes.push({...newNode, x: position.x, y: position.y});
-    this.updateNodes(this.positionnedNodes);
-    this.updateLinksPositions(this.positionnedNodes);
+      .attr('x1', (d: any) => `${this.networkService.getLinkNodes(this.positionnedNodes, d).from?.x}px`)
+      .attr('x2', (d: any) => `${this.networkService.getLinkNodes(this.positionnedNodes, d).to?.x}px`)
+      .attr('y1', (d: any) => `${this.networkService.getLinkNodes(this.positionnedNodes, d).from?.y}px`)
+      .attr('y2', (d: any) => `${this.networkService.getLinkNodes(this.positionnedNodes, d).to?.y}px`)
   }
 }
--- src/app/services/network.service.ts
+++ src/app/services/network.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as d3 from 'd3';
 import { NetworkLink, NetworkNode, PositionnedNode } from '../shared/models/network';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable({
   providedIn: 'root'
@@ -13,14 +14,13 @@ export class NetworkService {
     d3.select(element).raise().classed("dragging", true);
   }
 
-  public dragged = (element: any, event: DragEvent) => {
-    console.log(event.x)
+  public dragged = (element: any, pos: {x: number, y: number}) => {
     d3.select(element)
     .attr("x", (d: any) => {
-      return d.x = +event.x;
+      return pos.x;
     })
     .attr("y", (d: any) => {
-      return d.y = +event.y;
+      return pos.y;
     });
   }
 
@@ -33,5 +33,23 @@ export class NetworkService {
       from: (nodes || []).find(n => n.id === link.from),
       to: (nodes || []).find(n => n.id === link.to),
     }
+  }
+
+  public createNode = (event: PointerEvent): PositionnedNode => {
+    const id: string = uuidv4();
+    const newNode: PositionnedNode = {
+      album: {
+        name: 'Unknown',
+        coverUrl: 'https://i.scdn.co/image/ab67616d00001e0285e087e2715d317c33f71a31'
+      },
+      id: id,
+      x: event.offsetX, y: event.offsetY
+    }
+    return newNode;
+  }
+
+  public createLink = (id1: string, id2: string): NetworkLink => {
+    const link: NetworkLink = { from: id1, to: id2 };
+    return link;
   }
 }
